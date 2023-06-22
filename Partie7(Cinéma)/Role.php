4b442f64db0ea6025d652298196e04c93d159314
AlexandreSama <alexandrehainy12@gmail.com>
2023-06-22 09:20:20 +0200

    remove useless stuff

--- Partie7(Cinéma)/Role.php
+++ Partie7(Cinéma)/Role.php
@@ -3,16 +3,12 @@
 
     Class Role{
         public string $nom_personnage;
-        public Acteur $acteur;
-        public Film $film;
 
         public function __construct(string $nom_personnage, Film $film, Acteur $acteur)
         {
             $this->nom_personnage = $nom_personnage;
-            $this->film = $film;
-            $this->acteur = $acteur;
-            $acteur->addFilmToActor($this->film);
-            $this->film->casting += [$acteur->prenom . " " . $acteur->nom => $nom_personnage];
+            $acteur->addFilmToActor($film);
+            $film->casting += [$acteur->prenom . " " . $acteur->nom => $nom_personnage];
         }
     }
 
