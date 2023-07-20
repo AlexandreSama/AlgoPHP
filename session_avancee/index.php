71ac7d313c0251ec27b8dd5bbe064ebcf877768c
AlexandreSama <alexandrehainy12@gmail.com>
2023-07-20 10:46:23 +0200

    fix logo redirection

--- session_avancee/index.php
+++ session_avancee/index.php
@@ -16,7 +16,7 @@ session_start();
     <div class="container text-center">
         <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
-                <a class="navbar-brand" href="#">Poppy</a>
+                <a class="navbar-brand" href="./index.php">Poppy</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
--- session_avancee/recap.php
+++ session_avancee/recap.php
@@ -15,7 +15,7 @@ session_start();
 <body>
     <nav class="navbar navbar-expand-lg bg-body-tertiary">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">Poppy</a>
+            <a class="navbar-brand" href="./index.php">Poppy</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
