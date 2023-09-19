ca8a4c3382668c09c57850f9562fb96fd0071a51
AlexandreSama <alexandrehainy12@gmail.com>
2023-09-19 09:47:25 +0200

    fix

--- forumPlateau/view/layout.php
+++ forumPlateau/view/layout.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -58,13 +59,13 @@
         ?>
     </div>
     </nav>
-    <?php
-    if (App\Session::getFlash('success') || App\Session::getFlash('error')) {
-        echo '<h3 class="message" style="color: red">' . App\Session::getFlash('error') .
-            '</h3><h3 class="message" style="color: green">' . App\Session::getFlash('success') . '</h3>';
-    }
-    ?>
     <main class="main-content">
+        <?php
+        if (App\Session::getFlash('success') || App\Session::getFlash('error')) {
+            echo '<h3 class="message" style="color: red">' . App\Session::getFlash('error') .
+                '</h3><h3 class="message" style="color: green">' . App\Session::getFlash('success') . '</h3>';
+        }
+        ?>
         <?= $page ?>
     </main>
 
